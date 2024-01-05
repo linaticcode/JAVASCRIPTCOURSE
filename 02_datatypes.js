d975fb2cb389dfebc9bb979f787f45dae834a095
linaticcode <ss21eeb0a59@student.nitw.ac.in>
2024-01-05 12:55:40 +0530

    updated 5th JAN

---
+++ 02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs not browser
+// browser binds the js engine within it
+
+// console.log(3 + 3); console.log("hitesh")
+// code readabilty is not there in the above line
+
+
+// console.log("Hitesh")
+
+let name = "hitesh"
+let age  = 18
+let isLoggedIn = false
+
+// primitive data types
+
+// number => 2 to power 53
+// bigint 
+// string => "1" 
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+ 
+
