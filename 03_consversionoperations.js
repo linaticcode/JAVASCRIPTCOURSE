73fb8c505930035dbb4893cdf96dbcd5ae2e7495
linaticcode <ss21eeb0a59@student.nitw.ac.in>
2024-01-12 19:37:01 +0530

    upadted

---
+++ 03_consversionoperations.js
@@ -0,0 +1,57 @@
+let score  = "33abc"
+
+// console.log(typeof score);
+// can also write as
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//  not a number (NaN)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 ;  false => 0;
+
+let islog = 1;
+
+let boole = Boolean(islog);
+// console.log(boole);
+
+// 1 => true; 0 => false;
+// "" => false
+// "hitesh" => true;
+
+// ***************************** Operations ***********************************
+
+let value =  3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+
+let str3  = str1 + str2 ;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// don't write such messy code which are difficult to read 
+
+// console.log(+true);
+// // =>1
+// console.log(+"");
+// => 0
+let num1 ,  num2 , num3;
+
+num1 = num2 = num3 = 2 + 2
+
+// read mdn postfix and prefix;
